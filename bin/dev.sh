1fa22b3979215799fecd37bd631d3d2527ae13d8
Jeff Silva <jeferson.i.silva@gmail.com>
2026-01-02 22:18:18 -0300

    feat: - Configuração do ambiente de desenvolvimento para usar Octane com FrankenPHP. - Adição do Caddyfile para o servidor FrankenPHP. - Limpeza de linhas comentadas desnecessárias no script de desenvolvimento.

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-export SERVICE_APP_INIT="php artisan serve --host=0.0.0.0 --port=80"
-# export SERVICE_APP_INIT="php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=80 --admin-port=2019 --caddyfile=/app/Caddyfile --watch"
+# export SERVICE_APP_INIT="php artisan serve --host=0.0.0.0 --port=80"
+export SERVICE_APP_INIT="php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=80 --admin-port=2019 --caddyfile=/app/Caddyfile --watch"
 docker compose up --build --force-recreate --remove-orphans
-
-# docker compose up -d
-# (cd service-supabase/src && docker compose up)
